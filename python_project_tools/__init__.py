5715587d0bcdf96a058a5785dd92eb42cb1b8a66
WoLpH <Rick@Fawo.nl>
2012-08-13 10:40:37 +0200

    added readme

--- python_project_tools/__init__.py
+++ python_project_tools/__init__.py
@@ -11,30 +11,4 @@ __author_email__ = 'Rick.van.Hattem@Fawo.nl'
 __description__ = ('Python Project Tools is a package that makes developing '
     'and deploying proper Python packages easier')
 __url__ = 'https://github.com/WoLpH/python-project-tools/'
-__long_description__ = '''
-Python Project Tools is a package that makes developing and deploying proper
-Python packages easier.
-
-This package can be a starting point for new applications with templates for:
-- Sphinx documentation
-- Automated doctesting through nose
-- Travis configuration for automated testing of the nose tests
-- ReadTheDocs compatible configuration
-- Uniform variables like author, website and more for your `setup.py` and
-  Sphinx configuration.
-- A complete `setup.py` including `MANIFEST.in` and documentated template
-  explaining how to use `setup_requires`, `packages`, `entry_points` and more.
-- Git repository with automatic Github project creation
-- Creating and activating a virtualenv using virtualenvwrapper
-
-It also has short commands for deploying your code:
-- automatically running the test suite before deploying
-- deploying your code to the Python Package Index (PyPI)
-- deploying your code to git including creating a new tag for the release
-- deploying the documentation to PyPI
-- deploying the documentation to ReadTheDocs
-
-While deploying it is possible to automatically sign the releases (both Git
-and PyPI) with your own GPG key.
-'''
 
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author=python_project_tools.__author__,
     author_email=python_project_tools.__author_email__,
     description=python_project_tools.__description__,
-    long_description=python_project_tools.__long_description__,
+    long_description=open('README.rst').read(),
     license=python_project_tools.__license__,
     packages=['python_project_tools'],
     test_suite='nose.collector',
